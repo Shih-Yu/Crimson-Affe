a142fcba62bfe437d51e1c17a75887e780f2de2e
Iris Nathan <iris.nathan@g.austincc.edu>
2021-08-04 14:23:18 -0500

    IN Hardhat setup

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: process.env.MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.POLYGON_MAINNET,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  solidity: "0.8.4",
+};
