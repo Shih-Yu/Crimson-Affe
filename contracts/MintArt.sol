46b9bd45a8758d0ae0beb7176bc35f2ba01fb3da
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-08-05 14:02:54 -0500

    add comments to MintArt

--- contracts/MintArt.sol
+++ contracts/MintArt.sol
@@ -1,31 +1,39 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.6;
 
-// utilities / functions imported for use
+// Using _mint()form ERC721
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// Using _setTokenURI() from ERC721RUIStorage 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// Using Counters for incrementing ID and getting current Id
 import "@openzeppelin/contracts/utils/Counters.sol";
-
 import "hardhat/console.sol";
 
 
 contract MintArt is ERC721URIStorage{
+  // Calling Counter utility
   using Counters for Counters.Counter;
+  // Assigning _nftId as a Counter type variable
   Counters.Counter private _nftId;
-  address affeContractAderess;
+  // Declaring marketplace contract address
+  address affeContractAddress;
+  // Declaring owner address
   address owner;
   
 
+// Sets name and symbol of nft token to marketplace address when MintArt is deployed
   constructor(address _affeContractAddress) ERC721("AffeCoin", "AFC") {
-    affeContractAddress = _affeContractAddress;
-    owner = msg.sender; // assign the address that deployed this contract
+    affeContractAddress = _affeContractAddress; //Assign state address to contract address of marketplace
+    owner = msg.sender; // Assign the address that deployed this contract
   }
+  
   function createNFT(string memory tokenUri) public returns(uint) {
+    // Increment nftId before assigning
     _nftId.increment();
-
+    // Update nftId 
     uint updatednftId = _nftId.current();
     _mint(owner, updatednftId); // 2 parameters address and a id
-    _setTokenURI(updatednftId, tokenUri);
+    _setTokenURI(updatednftId, tokenUri); 
     
     return updatednftId;
   }
