72fcb0d24b7a6bcac56925bacb96fca0d8064b10
Iris Nathan <iris.nathan@g.austincc.edu>
2021-08-05 19:54:27 -0500

    IN add more logic to market contract

--- contracts/AffeMarket.sol
+++ contracts/AffeMarket.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.6;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol"
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
@@ -17,20 +17,26 @@ contract AffeMarket is ReentrancyGuard {
     owner = payable(msg.sender);
   }
 
-
   struct AffeItem {
     uint tokenId;
     uint itemId;
     uint price;
     address mintArtContract;
-    address payable seller; 
+    address payable buyer; 
     address payable owner;
     bool sold;
   }
   
   mapping(uint => AffeItem) private affeItems;
 
-  event AffeItemCreated(uint indexed tokenId, uint indexed itemId, uint price, address indexed mintArtContract, address seller, address owner, bool sold);
+  event AffeItemCreated(
+    uint indexed tokenId, 
+    uint indexed itemId, 
+    uint price, 
+    address indexed mintArtContract, 
+    address buyer, 
+    address owner, 
+    bool sold);
 
   function getListingFee() public view returns(uint) {
     return listingFee;
@@ -52,8 +58,30 @@ contract AffeMarket is ReentrancyGuard {
     );
 
     IERC721(mintArtContract).transferFrom(msg.sender, address(this), tokenId);
-    emit AffeItemCreate(tokenId, itemId, price, mintArtContract, msg.sender, address(0), false);
-    
+    emit AffeItemCreate(
+      tokenId, 
+      itemId, 
+      price, 
+      mintArtContract, 
+      msg.sender, 
+      address(0), 
+      false);   
   }
+
+  function createAffeSale(address mintArtContract, uint itemId) public payable nonReentrant {
+    uint price = affeItems[itemId].price;
+    uint tokenId = affeItems[itemId].tokenId;
+    require(msg.value == price, "Need more funds to buy item");
+
+    affeItems[itemId].buyer.transfer(msg.value);
+
+    IERC721(mintArtContract).transferFrom(address(this), msg.sender, tokenId);
+
+    affeItems[itemId].owner = payable(msg.sender); // .owner is from struct
+    affeItems[itemId].sold = true;
+    itemSold.increment();
+    payable(owner).transfer(listingFee); // this owner is from state variable
+  }
+
 
 }
