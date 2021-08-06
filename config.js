bc724a7df40f0f4b45451cdd60976032cbd425b9
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-08-06 14:55:47 -0500

    add contract addresses for Mumbai testnet

--- config.js
+++ config.js
@@ -1,2 +1,7 @@
-export const mintArtAddress ="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+// local
+export const mintArtAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 export const affeMarketAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
+// Mumbai
+export const mintArtAddress ="0x2b19c3720860bad5085927A460a7F3dD337C6F12"
+export const affeMarketAddress="0x66b9E84E0D55f67d4898218de9E69F7c7E92b68C"
