5a92c0c5c82ba9c2abd23e6625fe90f388018d60
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-08-08 13:50:38 -0500

    add wallet connection function

--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -3,9 +3,22 @@ import { Nav, Navbar, Container } from "react-bootstrap";
 import logo from "../../assets/crimson_affe.png";
 import { myStyle } from './styles/header';
 import { ROUTES } from "../../utils/constants/routes";
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+import { affeMarketAddress, mintArtAddress } from "../../config"
+import AffeMarket from "../../artifacts/contracts/AffeMarket.sol/AffeMarket.json";
+import MintArt from "../../artifacts/contracts/MintArt.sol/MintArt.json";
 
 export default function Header() {
   
+  async function walletConnect() {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const affeContract = new ethers.Contract(affeMarketAddress, AffeMarket.abi, signer);
+    const mintArtContract = new ethers.Contract(mintArtAddress, MintArt.abi, provider)
+  }
   return (
     <div>
       <Navbar style={myStyle}>
@@ -32,7 +45,7 @@ export default function Header() {
               </Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="">
+              <Nav.Link href="" onClick={walletConnect}>
                 <span style={{ color: "#fdbe02" }}>
                   <i class="fas fa-wallet fa-3x"></i>
                 </span>
