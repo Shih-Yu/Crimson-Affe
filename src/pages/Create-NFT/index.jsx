f138e84afa48abbfb389b6337a67828cdf7f0909
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-08-09 12:29:04 -0500

    update createNFT page

--- src/pages/Create-NFT/index.jsx
+++ src/pages/Create-NFT/index.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import { Form, Button, FloatingLabel } from "react-bootstrap";
-import { pageTemplate, form, h1 } from './styles/createNFT'
+import { pageTemplate, form, h1 } from "./styles/createNFT";
+import { useState } from "react";
+import { ethers } from "ethers";
+import { RouterBrowser as Redirect } from "react-router-dom";
+import { NFTStorage, File } from "nft.storage";
 
+// NFT.Storage connection
+const apiKey = process.env.NFTSTORAGE_KEY;
+const client = new NFTStorage({ token: apiKey });
 
 export default function CreateNFT() {
+  const [file, setFile] = useState(null);
+  const [formInput, setFormInput] = useState({
+    name: "",
+    artPiece: "",
+    price: "",
+    description: "",
+  });
+
+  async function onChange(event) {
+    const files = event.target.files[0];
+    try {
+      const metadata = await client.store({
+        name: "",
+        artPiece: "",
+        price:"",
+        description: "",
+        image: new File(files)
+      })
+    }
+  }
+  
   return (
     <div style={pageTemplate}>
       <fieldset style={form}>
@@ -22,6 +50,11 @@ export default function CreateNFT() {
             <Form.Control type="text" placeholder="Art Name" />
           </Form.Group>
 
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Price In Matic</Form.Label>
+            <Form.Control type="text" placeholder="Price" />
+          </Form.Group>
+
           <FloatingLabel controlId="floatingTextarea2" className="mb-2" label="Description">
             <Form.Control
               as="textarea"
@@ -33,7 +66,7 @@ export default function CreateNFT() {
             <Form.Label>Upload File</Form.Label>
             <Form.Control type="file" />
           </Form.Group>
-          <Button style={ { backgroundColor: "#fdbe02", border: "none" }} type="submit">
+          <Button style={{ backgroundColor: "#fdbe02", border: "none" }} type="submit">
             Submit
           </Button>
         </Form>
