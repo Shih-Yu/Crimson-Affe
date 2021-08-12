3045739be127d19de22c1891c5eec9e92b97e5fb
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-08-12 16:33:52 -0500

    update

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // local
-export const mintArtAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-export const affeMarketAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3";
+//export const mintArtAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+//export const affeMarketAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 // Mumbai
-//export const mintArtAddress ="0x2b19c3720860bad5085927A460a7F3dD337C6F12";
-//export const affeMarketAddress="0x66b9E84E0D55f67d4898218de9E69F7c7E92b68C";
+export const mintArtAddress ="0x2b19c3720860bad5085927A460a7F3dD337C6F12";
+export const affeMarketAddress="0x66b9E84E0D55f67d4898218de9E69F7c7E92b68C";
 
 // Goerli
 // export const mintArtAddress ="0x349d4d28eB9B46c3c90c206619f922403bBa6057";
--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -1,24 +1,10 @@
 import React from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
 import logo from "../../assets/crimson_affe.png";
-import { myStyle } from './styles/header';
+import { myStyle } from "./styles/header";
 import { ROUTES } from "../../utils/constants/routes";
-import { ethers } from "ethers";
-import Web3Modal from "web3modal";
-import { affeMarketAddress, mintArtAddress } from "../../config"
-import AffeMarket from "../../artifacts/contracts/AffeMarket.sol/AffeMarket.json";
-import MintArt from "../../artifacts/contracts/MintArt.sol/MintArt.json";
 
 export default function Header() {
-  
-  async function walletConnect() {
-    const web3Modal = new Web3Modal();
-    const connection = await web3Modal.connect();
-    const provider = new ethers.providers.Web3Provider(connection);
-    const signer = provider.getSigner();
-    await new ethers.Contract(affeMarketAddress, AffeMarket.abi, signer);
-    await new ethers.Contract(mintArtAddress, MintArt.abi, provider)
-  }
   return (
     <div>
       <Navbar style={myStyle}>
@@ -42,13 +28,6 @@ export default function Header() {
             <Nav.Item>
               <Nav.Link href={ROUTES.createNFT} style={myStyle}>
                 Create NFT
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="" onClick={walletConnect}>
-                <span style={{ color: "#fdbe02" }}>
-                  <i className="fas fa-wallet fa-3x"></i>
-                </span>
               </Nav.Link>
             </Nav.Item>
           </Nav>
