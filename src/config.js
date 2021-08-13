dfff575504af01d02ed6ca96e0ac30eb57b73d3d
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-08-13 11:57:02 -0500

    update config

--- src/config.js
+++ src/config.js
@@ -1,13 +1,11 @@
 // local
-export const mintArtAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-export const affeMarketAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3";
+//export const mintArtAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+//export const affeMarketAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 // Mumbai
-//export const mintArtAddress ="0x2b19c3720860bad5085927A460a7F3dD337C6F12";
-//export const affeMarketAddress="0x66b9E84E0D55f67d4898218de9E69F7c7E92b68C";
+export const mintArtAddress = "0x5749018642B42506655b2DD7b2E1bFA2cF85B22e";
+export const affeMarketAddress = "0x98d85710a190BE3e61FB61f146A0280286c3d9ea";
 
 // Goerli
 // export const mintArtAddress ="0x349d4d28eB9B46c3c90c206619f922403bBa6057";
 // export const affeMarketAddress="0x2564b7E11090C3578F7D186D82c2aC2004A81031";
-
-
