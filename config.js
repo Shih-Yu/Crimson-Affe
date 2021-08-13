5c1a98836dd44b0ba95ee93a21b6da77a31735cf
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-08-13 11:58:42 -0500

    update config

--- config.js
+++ config.js
@@ -3,11 +3,9 @@
 //export const affeMarketAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 // Mumbai
-export const mintArtAddress ="0x3da76c7C62bFddCDe97a8b518d709f0929EaEF84";
-export const affeMarketAddress="0xcFE12a529F83a633f52eFaDb13d63845e674D186";
+export const mintArtAddress = "0x5749018642B42506655b2DD7b2E1bFA2cF85B22e";
+export const affeMarketAddress = "0x98d85710a190BE3e61FB61f146A0280286c3d9ea";
 
 // Goerli
 // export const mintArtAddress ="0x349d4d28eB9B46c3c90c206619f922403bBa6057";
 // export const affeMarketAddress="0x2564b7E11090C3578F7D186D82c2aC2004A81031";
-
-
