db23cd27e64050f54026d4868cddc760e2cceb85
Iris Nathan <iris.nathan@g.austincc.edu>
2021-08-14 16:27:31 -0500

    IN add images export page

--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -1,47 +1,58 @@
-import React, { useEffect, useState } from "react";
-import { ethers } from "ethers";
-import Web3modal from "web3modal";
-import axios from "axios";
-import { pageTemplate } from "./styles/gallery";
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import Web3modal from 'web3modal';
+import axios from 'axios';
+import { pageTemplate } from './styles/gallery';
+import images from '../assets/images.js';
 
-import { affeMarketAddress, mintArtAddress } from "../../config";
-import AffeMarket from "../../artifacts/contracts/AffeMarket.sol/AffeMarket.json";
-import MintArt from "../../artifacts/contracts/MintArt.sol/MintArt.json";
+import { affeMarketAddress, mintArtAddress } from '../../config';
+import AffeMarket from '../../artifacts/contracts/AffeMarket.sol/AffeMarket.json';
+import MintArt from '../../artifacts/contracts/MintArt.sol/MintArt.json';
+
 
 export default function Gallery() {
   const [nfts, setNfts] = useState([]);
-  const [upload, setUpload] = useState("not-loaded");
+  const [upload, setUpload] = useState('not-loaded');
 
   useEffect(() => {
     loadNfts();
   }, []);
 
   async function loadNfts() {
-    const provider = new ethers.providers.JsonRpcProvider(process.env.REACT_APP_MUMBAI_URL);
-    const tokenContract = new ethers.Contract(mintArtAddress, MintArt.abi, provider);
-    const marketContract = new ethers.Contract(affeMarketAddress, AffeMarket.abi, provider);
+    const provider = new ethers.providers.JsonRpcProvider(
+      process.env.REACT_APP_MUMBAI_URL
+    );
+    const tokenContract = new ethers.Contract(
+      mintArtAddress,
+      MintArt.abi,
+      provider
+    );
+    const marketContract = new ethers.Contract(
+      affeMarketAddress,
+      AffeMarket.abi,
+      provider
+    );
     const data = await marketContract.getAffeItems();
 
     const items = await Promise.all(
       data.map(async (i) => {
         const tokenUri = await tokenContract.tokenURI(i.tokenId);
         const meta = await axios.get(tokenUri);
-        let price = ethers.utils.formatUnits(i.price.toString(), "ethers");
+        let price = ethers.utils.formatUnits(i.price.toString(), 'ethers');
         let item = {
           price,
           tokenId: i.tokenId.toNumber(),
           seller: i.seller,
           owner: i.owner,
-          image: meta.data.image,
+          //image: meta.data.image,
           name: meta.data.name,
-          artPiece: meta.data.artPiece,
           description: meta.data.description,
         };
         return item;
       })
     );
     setNfts(items);
-    setUpload("loaded");
+    setUpload('loaded');
   }
 
   async function buyNft(nft) {
@@ -50,37 +61,44 @@ export default function Gallery() {
     const provider = new ethers.providers.Web3Provider(connection);
     const signer = provider.getSigner();
 
-    const tokenContract = new ethers.Contract(affeMarketAddress, AffeMarket.abi, signer);
-    const price = ethers.utils.parseUnits(nft.price.toString(), "ethers");
+    const tokenContract = new ethers.Contract(
+      affeMarketAddress,
+      AffeMarket.abi,
+      signer
+    );
+    const price = ethers.utils.parseUnits(nft.price.toString(), 'ethers');
 
-    const transaction = await tokenContract.createAffeSale(mintArtAddress, nft.tokenId, {
-      value: price,
-    });
+    const transaction = await tokenContract.createAffeSale(
+      mintArtAddress,
+      nft.tokenId,
+      {
+        value: price,
+      }
+    );
     await transaction.wait();
     loadNfts();
   }
 
-  if (upload === "loaded" && !nfts.length) {
-    return <h1 className="text-center px-20 py-10">No NFTs available</h1>;
-  }
+  // if (upload === 'loaded' && !nfts.length) {
+  //   return <h1 className='text-center px-20 py-10'>No NFTs available</h1>;
+  // }
 
   return (
     <>
       <div style={pageTemplate}>
-        <div className="card-group">
-          {nfts.map((nft, index) => (
-            <div className="card my-3 mx-3" key={index}>
-              <img src={nft.image} className="card-img-top" alt="nft cards" />
-              <div className="card-body">
-                <h5 className="card-title">{nft.artPiece}</h5>
-                <p className="card-text">{nft.description}</p>
-                <small className="text-muted">{nft.name}</small>
-                <p className="card-text">{nft.price} Ethers</p>
+        <div className='card-group'>
+          {images.map((nft, index) => (
+            <div className='card my-3 mx-3' key={index}>
+              <img src={nft.src} className='card-img-top' alt='nft cards' />
+              <div className='card-body'>
+                <h5 className='card-title'>{nft.title}</h5>
+                <p className='card-text'>{nft.description}</p>
+                <p className='card-text'>{nft.price} Ethers</p>
               </div>
-              <div className="card-footer">
+              <div className='card-footer'>
                 <button
-                  className="btn btn-small btn"
-                  style={{ backgroundColor: "#fdbe02", border: "none" }}
+                  className='btn btn-small btn'
+                  style={{ backgroundColor: '#fdbe02', border: 'none' }}
                   onClick={() => buyNft(nft)}
                 >
                   Buy
@@ -89,6 +107,27 @@ export default function Gallery() {
             </div>
           ))}
         </div>
+        {/* <div className='card-group'>
+          {photos.map((nft, index) => (
+            <div className='card my-3 mx-3' key={index}>
+              <img src={nft} className='card-img-top' alt='nft cards' />
+              <div className='card-body'>
+                <h5 className='card-title'>{nft.artPiece}</h5>
+                <p className='card-text'>{nft.description}</p>
+                <p className='card-text'>{nft.price} Ethers</p>
+              </div>
+              <div className='card-footer'>
+                <button
+                  className='btn btn-small btn'
+                  style={{ backgroundColor: '#fdbe02', border: 'none' }}
+                  onClick={() => buyNft(nft)}
+                >
+                  Buy
+                </button>
+              </div>
+            </div>
+          ))}
+        </div> */}
       </div>
     </>
   );
